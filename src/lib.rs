882b9a4a60b9bc689334af4e4b09a9fd5fdea10e
zhenfei <zhenfei.zhang@hotmail.com>
2023-10-30 12:18:50 -0400

    update halo2 version

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,24 @@
-use halo2_proofs::circuit::AssignedCell;
-use halo2_proofs::arithmetic::FieldExt;
+#![allow(dead_code)]
+use halo2_proofs::{circuit::AssignedCell, halo2curves::ff::PrimeField};
 
 #[derive(Clone, Debug)]
-pub struct Limb<F: FieldExt> {
+pub struct Limb<F: PrimeField> {
     pub cell: Option<AssignedCell<F, F>>,
-    pub value: F
+    pub value: F,
 }
 
-impl<F:FieldExt> Limb<F> {
+impl<F: PrimeField> Limb<F> {
     pub fn new(cell: Option<AssignedCell<F, F>>, value: F) -> Self {
         Limb { cell, value }
     }
-    pub fn get_the_cell(&self) -> AssignedCell<F,F> {
+    pub fn get_the_cell(&self) -> AssignedCell<F, F> {
         self.cell.as_ref().unwrap().clone()
     }
 }
 
-#[derive (Debug)]
+#[derive(Debug)]
 pub struct GateCell {
-    pub cell: [usize;3],
+    pub cell: [usize; 3],
     pub name: String,
 }
 
